706d23dfd8d53f0d567f1a5ee3728678417ef837
feeblefruits <j.coetzee0@gmail.com>
2019-08-24 19:13:12 +0200

    Updated transition gallery content and order of pics

--- eswatini_files/js/floating-box.js
+++ eswatini_files/js/floating-box.js
@@ -1,18 +1,18 @@
 var contentList = [
-	"Moses Mdluli sits on a cushion outside. His arms crossed around his chest. He holds on tightly and coughs a couple of times. \"It's especially bad in the winter,\" he says.",
-	"His children sit on the porch and play a game of stone-toss-and-pick-up called kudoda. \"When I see them in the mornings, I\'m alive.\", Mdluli shares.",
-	"The 55-year-old former mineworker from the twon Hlatikulu in Eswatini can\’t pace or walk more than 20 metres without wheezing. He often coughs up blood in the evenings.",
-	"Silicosis or Worker\'s lung, as it\'s commonly known, made him redundant as a worker 14 years ago. \"They don't want skorokoro,\” he says talking about the South African gold mines.",
-	"These days he can\'t lift a bucket of water. The Swati man started as a helper and eventually worked his way up to a line manager. \“As soon as they see you coughing they chase you away.\"",
-	"Mdluli only has one of his lungs left and wheezes when he trudges. \“The one is fucked up,\” he says angrily.",
-	"Close to Manzini in Eswatini lives Isaac Lukhele (75) who started working in South Africa\’s gold mines in 1962.",
-	"\“When I was retrenched from the mines in 2004, the doctors told me I will be compensated in Eswatini. But nothing has happened,\” Lukhele recalls.",
-	"He says that in 1962 he used to earn 25 cents a day. \“Treatment wasn\’t really good. We were not treated like human beings,\” he says. There used to be 50 people in one dormatory.",
-	"Today, Lukhele supports his family by farming with a handful of goats and chickens. He\'s saving money to build a shelter for his livestock and receives a monthly pension of R400 from government.",
-	"Dumisile Dlamini is from Hlathikhulu's occupational health service centre Eswatini. She explains that treatments are expensive and difficult to come by for struggling patients.",
-	"\“In Eswatini you’ll find that many of the patients are difficult to reach,\” explains Dlamini. \“Many of them are older.",
-	"Tuberculosis is very common among those who\’ve contracted silicosis and can contaminate close friends and family. \“As you know men are leaders of the households.\” Dlamini says.",
-	"Mdluli and Lukhele are part of tens of thousands former mine workers. Of those surviving today, many are sickly and live in remote, rural areas across Southern Africa."
+    "Tens of thousands of former mineworkers and their families survive today; many are sickly and live in remote, rural areas across Southern Africa."
+	"One of them is Moses Mdluli. It\’s a hot winter\’s day and the mountains of Eswatini sprawl out in front of him. With his arms crossed over his chest, he holds on tightly and coughs painfully. \“It\’s especially bad in the winter,\” he says."
+    "His children sit on the porch and play a game of stone-toss-and-pick-up called kudoda. \"When I see them in the mornings, I\’m alive,\” Mdluli says.",
+	"The mountain air is fresh but the 55-year-old former mineworker from the town of Hlathikhulu can\’t walk more than 20m without wheezing. In the evenings he coughs up blood and often has to catch his breath mid-sentence.",
+	"Small dust particles breathed in while cutting and grinding through the depth of the Earth at the Western Areas gold mine have led to him contracting silicosis. He was made redundant 14 years ago. \“The mines don’t want iskorokoro,\” he says.",
+	"These days Mdluli can\'t lift a bucket of water. The Swati man started as a helper in the Joel Mine in the Free State and eventually worked his way up to line manager. \“As soon as they see you coughing they chase you away.\” he says.",
+	"Mdluli only has one lung left and wheezes when he trudges. \“The one is fucked up,\” he says angrily.",
+	"Isaac Lukhele lives close to Manzini in Eswatini. He started working at South Africa\’s gold mines in 1962. He worked at 15 different mines, often going deeper than 2,000m.",
+	"\“When I was retrenched from the mines in 2004, the doctors told me I will be compensated in Eswatini. But nothing happened,\” Lukhele recalls.",
+	"\“Treatment wasn\’t really good [at the mines]. We were not treated like human beings,\” he says. \“There used to be 50 people in one dormitory.\”",
+    "Today, Lukhele supports his family through farming. He has a handful of goats and chickens and he\’s saving money to build a shelter for his livestock. Lukhele receives a monthly pension of R400 from the Eswatini government.",
+    "Dumisile Dlamini works at Hlathikhulu\’s occupational health service centre in Eswatini. She says that treatments are expensive and difficult to come by for struggling patients.",
+    "\“In Eswatini, you\’ll find that many of the patients are difficult to reach,\” she says. \“Many of them are older and sick.\”",
+    "Tuberculosis is very common among people who\’ve contracted silicosis and can be passed on to close friends and family. \“As you know, men are leaders of the households.\” Dlamini says."
 	];
 
 var ts = new TwoStep({
