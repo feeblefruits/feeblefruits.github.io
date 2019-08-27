dab4c859ea307b3b613c81e1103f4cbacfdbb68c
feeblefruits <j.coetzee0@gmail.com>
2019-08-27 16:35:22 +0200

    Updated last edit subs

--- eswatini_files/js/floating-box.js
+++ eswatini_files/js/floating-box.js
@@ -1,18 +1,18 @@
 var contentList = [
-    "Tens of thousands of former mineworkers and their families survive today; many are sickly and live in remote, rural areas across Southern Africa. <p>Scroll down here to read about their stories</p>.",
+    "Tens of thousands of former mineworkers and their families survive today; many are sickly and live in remote, rural areas across Southern Africa. <bold>Scroll down to read their stories</bold>.",
   	"One of them is <p>Moses Mdluli</p>. It\’s a hot winter\’s day and the mountains of Eswatini sprawl out in front of him. With his arms crossed over his chest, he holds on tightly and coughs painfully. \“It\’s especially bad in the winter,\” he says.",
     "His children sit on the porch and play a game of stone-toss-and-pick-up called kudoda. \"When I see them in the mornings, I\’m alive,\” <p>Mdluli</p> says.",
   	"The mountain air is fresh but the 55-year-old former mineworker from the town of Hlathikhulu can\’t walk more than 20m without wheezing. In the evenings he coughs up blood and often has to catch his breath mid-sentence.",
-  	"Small dust particles breathed in while cutting and grinding through the depth of the Earth at the Western Areas gold mine have led to him contracting silicosis. He was made redundant 14 years ago. \“The mines don’t want iskorokoro,\” he says.",
-  	"These days <p>Mdluli</p> can\'t lift a bucket of water. The Swati man started as a helper in the <p>Joel Mine</p> in the Free State and eventually worked his way up to line manager. \“As soon as they see you coughing they chase you away.\” he says.",
-  	"<p>Mdluli</p> only has one lung left and wheezes when he trudges. \“The one is fucked up,\” he says angrily.",
+  	"Small dust particles breathed in while cutting and grinding through the depths of the Earth at the Western Areas gold mine have led to him contracting silicosis. He was made redundant 14 years ago. \“The mines don’t want iskorokoro,\” he says.",
+  	"These days <p>Mdluli</p> can\'t lift a bucket of water. The Swati man started as a helper in the <p>Joel Mine</p> in the Free State and eventually worked his way up to line manager. \“As soon as they see you coughing they chase you away,\” he says.",
+  	"<p>Mdluli</p> has only one lung left and wheezes when he trudges. \“The one is fucked up,\” he says angrily.",
   	"<p>Isaac Lukhele</p> lives close to Manzini in Eswatini. He started working at South Africa\’s gold mines in 1962. He worked at 15 different mines, often going deeper than 2,000m.",
-  	"\“When I was retrenched from the mines in 2004, the doctors told me I will be compensated in Eswatini. But nothing happened,\” Lukhele recalls.",
+  	"\“When I was retrenched from the mines in 2004, the doctors told me I will be compensated in Eswatini. But nothing happened,\” <p>Lukhele</p> recalls.",
   	"\“Treatment wasn\’t really good [at the mines]. We were not treated like human beings,\” he says. \“There used to be 50 people in one dormitory.\”",
     "Today, <p>Lukhele</p> supports his family through farming. He has a handful of goats and chickens and he\’s saving money to build a shelter for his livestock. <p>Lukhele</p> receives a monthly pension of R400 from the Eswatini government.",
-    "<p>Dumisile Dlamini</p> works at Hlathikhulu\’s occupational health service centre in Eswatini. She says that treatments are expensive and difficult to come by for struggling patients.",
+    "<p>Dumisile Dlamini</p> works at Hlathikhulu\’s occupational health service centre in Eswatini. She says that treatments are expensive and difficult to access for struggling patients.",
     "\“In Eswatini, you\’ll find that many of the patients are difficult to reach,\” she says. \“Many of them are older and sick.\”",
-    "Tuberculosis is very common among people who\’ve contracted silicosis and can be passed on to close friends and family. \“As you know, men are leaders of the households.\” <p>Dlamini</p> says."
+    "Tuberculosis is very common among people who\’ve contracted silicosis and can be passed on to close friends and family. \“As you know, men are leaders of the households,\” <p>Dlamini</p> says."
 	];
 
 var ts = new TwoStep({
--- lesotho_files/js/floating-box.js
+++ lesotho_files/js/floating-box.js
@@ -1,11 +1,11 @@
 var contentList = [
-	"<p>Monokoa Lepota</p> is 61 years old from Maseru in Lesotho. He grows maize, pumpkins and, sometimes, wheat. He normally harvests between seven and eight bags of produce. <p>Scroll to read about his story</p>.",
+	"<p>Monokoa Lepota</p> is a 61-year-old from Maseru in Lesotho. He grows maize, pumpkins and, sometimes, wheat. He normally harvests between seven and eight bags of produce. <bold>Scroll to read his story</bold>.",
 	"He remembers it being unbearably hot and dusty underground in the mines, they would spray underground with water every two weeks to try to get the dust to settle.",
 	"<p>Lepota</p> also provides for his two married sons who have moved out of the village. If he receives a payout in the settlement, he intends to buy a milling machine. This would allow him to mill the produce for the village and earn an adequate income.",
 	"Medication is expensive and — for many people living in rural areas — difficult to come by.",
 	"<p>Mankosana Makhorolane</p> lost her husband, <p>Moliki</p>, who used to work in the mines, three years ago. He was 51 years old and used to make a four-hour journey home every month to visit his wife and children, always bringing money with him.",
 	"<p>Moliki</p> was released from the mines in 2010, following a silicosis diagnosis. His condition began worsening three years after he stopped working, and he died in 2016.",
-	"<p>Mankosana</p> says <p>Moliki</p> was distraught that he was no longer able to work at the mine and provide for his family and that \“his lungs were no longer to carry him\”."
+	"<p>Mankosana</p> says <p>Moliki</p> was distraught that he was no longer able to work at the mine and provide for his family and that \“his lungs were no longer able to carry him\”."
 	];
 
 var ts = new TwoStep({
